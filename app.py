5e392e7194f03db69eca29e69871e67621692271
nishal-s <nishalking17@gmail.com>
2026-04-06 21:45:10 +0530

    jenkins file changes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'warning'
 login_manager.init_app(app)
-
+#THis is just a comment
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
